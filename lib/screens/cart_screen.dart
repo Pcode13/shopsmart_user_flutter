9dc4f65cdfcaf228e39e468a1549d1787eb0ab9d
Pragati Veer <92431808+Pcode13@users.noreply.github.com>
2025-04-02 15:08:32 +0530

    Create the empty cart screen

--- lib/screens/cart_screen.dart
+++ lib/screens/cart_screen.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:shopsmart_user/services/assets_manager.dart';
+import 'package:shopsmart_user/widgets/empty_bag.dart';
 import 'package:shopsmart_user/widgets/title_text.dart';
 
 class CartScreen extends StatelessWidget {
-const CartScreen({ Key? key }) : super(key: key);
+  const CartScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: TitlesTextWidget(label: "CartScreen"),
+        child: EmptyBagWidget(
+          imagePath: AssetsManager.shoppingBasket,
+          title: "Your cart is empty",
+          subtitle:
+              "Looks like your cart is empty add something and make me happy",
+          buttonText: "Shop Now",
+        ),
       ),
     );
   }
