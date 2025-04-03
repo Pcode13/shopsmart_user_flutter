2131acf29fa5474729a6babfe2707745410b49c9
Pragati Veer <92431808+Pcode13@users.noreply.github.com>
2025-04-03 11:08:39 +0530

    Create the Product Model

---
+++ lib/models/product_model.dart
@@ -0,0 +1,18 @@
+class ProductModel {
+  final String productId;
+  final String productTitle;
+  final String productDescription;
+  final String productImageUrl;
+  final String productCategory;
+  final String productPrice;
+  final String productQuantity;
+  ProductModel({
+    required this.productId,
+    required this.productTitle,
+    required this.productDescription,
+    required this.productImageUrl,
+    required this.productCategory,
+    required this.productPrice,
+    required this.productQuantity,
+  });
+}
